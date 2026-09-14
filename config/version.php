889329953cb2a77713f422531dc02a627491b45d
beingniloy <wakeupniloy@gmail.com>
2026-09-14 15:11:29 +0600

    Update changes

--- config/version.php
+++ config/version.php
@@ -1,5 +1,5 @@
 <?php
-if (!defined('APP_VERSION')) define('APP_VERSION', 'v1.0.6');
+if (!defined('APP_VERSION')) define('APP_VERSION', 'v1.0.7');
 if (!defined('APP_BUILD_DATE')) define('APP_BUILD_DATE', '2026-09-14');
 if (!defined('APP_REPO')) define('APP_REPO', 'beingniloy/smslink');
 if (!defined('APP_APK_URL')) define('APP_APK_URL', 'https://github.com/beingniloy/smslink/releases/download/v1.0.0/SMSLink-v1.0.0.apk');
